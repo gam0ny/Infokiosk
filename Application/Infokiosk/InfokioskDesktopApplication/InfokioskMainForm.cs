c40a3b23cc05a85f9df5ab3025ad8d94dcc073a1
DariaSiomukha <50360110+DariaSiomukha@users.noreply.github.com>
2019-05-14 14:24:10 +0300

    Set relative path to all images

--- Application/Infokiosk/InfokioskDesktopApplication/InfokioskMainForm.cs
+++ Application/Infokiosk/InfokioskDesktopApplication/InfokioskMainForm.cs
@@ -1,6 +1,7 @@
 ﻿using CustomControlLibrary.Entities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -10,27 +11,29 @@ namespace InfokioskDesktopApplication
     {
         public InfokioskMainForm()
         {
+            var contentPath = ConfigurationManager.AppSettings["ContentPath"];
+
             InitializeComponent();
             this.iblvRecentlyAdded.Title = "Новое";
-            this.iblvRecentlyAdded.CountItemsPerLine = 4;
+            this.iblvRecentlyAdded.CountItemsPerLine = 5;
             this.iblvRecentlyAdded.ImageBoxItemList = new List<ImageBoxItem> {
-                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/1.jpg", HasDocuments = true, HasVideo = false },
-                new ImageBoxItem { Category="Золотой Век", Title = "Тема 2", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/2.jpg", HasDocuments = true, HasVideo = true },
-                new ImageBoxItem { Category="Искусство", Title = "Тема 3", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/3.jpg", HasDocuments = false, HasVideo = false },
-                new ImageBoxItem { Category="XVI Век", Title = "Тема 4", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/4.jpg", HasDocuments = false, HasVideo = false },
-                new ImageBoxItem { Category="Наука", Title = "Тема 5", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/5.jpg", HasDocuments = true, HasVideo = false },
-                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/1.jpg", HasDocuments = true, HasVideo = false },
-                new ImageBoxItem { Category="Золотой Век", Title = "Тема 2", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/2.jpg", HasDocuments = true, HasVideo = true },
-                new ImageBoxItem { Category="Искусство", Title = "Тема 3", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/3.jpg", HasDocuments = false, HasVideo = false },
-                new ImageBoxItem { Category="XVI Век", Title = "Тема 4", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/4.jpg", HasDocuments = false, HasVideo = false },
-                new ImageBoxItem { Category="Наука", Title = "Тема 5", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/5.jpg", HasDocuments = true, HasVideo = false },
+                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl=string.Format("{0}/1.jpg", contentPath), HasDocuments = true, HasVideo = false },
+                new ImageBoxItem { Category="Золотой Век", Title = "Тема 2", ImageUrl=string.Format("{0}/2.jpg", contentPath), HasDocuments = true, HasVideo = true },
+                new ImageBoxItem { Category="Искусство", Title = "Тема 3", ImageUrl=string.Format("{0}/3.jpg", contentPath), HasDocuments = false, HasVideo = false },
+                new ImageBoxItem { Category="XVI Век", Title = "Тема 4", ImageUrl=string.Format("{0}/4.jpg", contentPath), HasDocuments = false, HasVideo = false },
+                new ImageBoxItem { Category="Наука", Title = "Тема 5", ImageUrl=string.Format("{0}/5.jpg", contentPath), HasDocuments = true, HasVideo = false },
+                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl=string.Format("{0}/1.jpg", contentPath), HasDocuments = true, HasVideo = false },
+                new ImageBoxItem { Category="Золотой Век", Title = "Тема 2", ImageUrl=string.Format("{0}/2.jpg", contentPath), HasDocuments = true, HasVideo = true },
+                new ImageBoxItem { Category="Искусство", Title = "Тема 3", ImageUrl=string.Format("{0}/3.jpg", contentPath), HasDocuments = false, HasVideo = false },
+                new ImageBoxItem { Category="XVI Век", Title = "Тема 4", ImageUrl=string.Format("{0}/4.jpg", contentPath), HasDocuments = false, HasVideo = false },
+                new ImageBoxItem { Category="Наука", Title = "Тема 5", ImageUrl=string.Format("{0}/5.jpg", contentPath), HasDocuments = true, HasVideo = false },
             };
 
             this.iblvPerCategory.Title = "IXX Век";
-            this.iblvPerCategory.CountItemsPerLine = 4;
+            this.iblvPerCategory.CountItemsPerLine = 5;
             this.iblvPerCategory.ImageBoxItemList = new List<ImageBoxItem> {
-                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/1.jpg", HasDocuments = true, HasVideo = false },
-                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl="c:/Users/ruslan/Desktop/Инфокиоск для выстовочного отдела/_Content/1.jpg", HasDocuments = true, HasVideo = false },
+                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl=string.Format("{0}/1.jpg", contentPath), HasDocuments = true, HasVideo = false },
+                new ImageBoxItem { Category="IXX Век", Title = "Тема 1", ImageUrl=string.Format("{0}/2.jpg", contentPath), HasDocuments = true, HasVideo = false },
             };
         }
 
