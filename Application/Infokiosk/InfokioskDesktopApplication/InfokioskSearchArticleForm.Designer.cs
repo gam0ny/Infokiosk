0dd8516e7a2c05603696ca738c4d5fc47f5c9b0b
gam0ny <tonks.alina@gmail.com>
2019-05-27 19:38:35 +0300

    Update InfokioskSearchArticleForm.Designer.cs

--- Application/Infokiosk/InfokioskDesktopApplication/InfokioskSearchArticleForm.Designer.cs
+++ Application/Infokiosk/InfokioskDesktopApplication/InfokioskSearchArticleForm.Designer.cs
@@ -30,8 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfokioskSearchArticleForm));
             this.tableLayoutPanelSearchCriteria = new System.Windows.Forms.TableLayoutPanel();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.tbxSearch = new System.Windows.Forms.TextBox();
             this.panelSearchCriteria = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.rbContent = new System.Windows.Forms.RadioButton();
@@ -41,6 +39,8 @@
             this.panelToolbar = new System.Windows.Forms.Panel();
             this.lblBack = new System.Windows.Forms.Label();
             this.lblExit = new System.Windows.Forms.Label();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.tableLayoutPanelSearchCriteria.SuspendLayout();
             this.panelSearchCriteria.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -54,44 +54,23 @@
             this.tableLayoutPanelSearchCriteria.ColumnCount = 2;
             this.tableLayoutPanelSearchCriteria.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90F));
             this.tableLayoutPanelSearchCriteria.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanelSearchCriteria.Controls.Add(this.btnSearch, 1, 1);
             this.tableLayoutPanelSearchCriteria.Controls.Add(this.tbxSearch, 0, 0);
+            this.tableLayoutPanelSearchCriteria.Controls.Add(this.btnSearch, 1, 0);
             this.tableLayoutPanelSearchCriteria.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanelSearchCriteria.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelSearchCriteria.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
             this.tableLayoutPanelSearchCriteria.Name = "tableLayoutPanelSearchCriteria";
             this.tableLayoutPanelSearchCriteria.RowCount = 2;
-            this.tableLayoutPanelSearchCriteria.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelSearchCriteria.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanelSearchCriteria.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanelSearchCriteria.Size = new System.Drawing.Size(926, 100);
+            this.tableLayoutPanelSearchCriteria.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanelSearchCriteria.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanelSearchCriteria.Size = new System.Drawing.Size(926, 50);
             this.tableLayoutPanelSearchCriteria.TabIndex = 7;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnSearch.Enabled = false;
-            this.btnSearch.Location = new System.Drawing.Point(836, 3);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(87, 94);
-            this.btnSearch.TabIndex = 4;
-            this.btnSearch.Text = "Найти";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
-            // 
-            // tbxSearch
-            // 
-            this.tbxSearch.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tbxSearch.Location = new System.Drawing.Point(3, 3);
-            this.tbxSearch.Name = "tbxSearch";
-            this.tbxSearch.Size = new System.Drawing.Size(827, 29);
-            this.tbxSearch.TabIndex = 2;
-            this.tbxSearch.TextChanged += new System.EventHandler(this.TbxSearch_TextChanged);
             // 
             // panelSearchCriteria
             // 
             this.panelSearchCriteria.Controls.Add(this.groupBox1);
             this.panelSearchCriteria.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelSearchCriteria.Location = new System.Drawing.Point(0, 100);
+            this.panelSearchCriteria.Location = new System.Drawing.Point(0, 50);
             this.panelSearchCriteria.Name = "panelSearchCriteria";
             this.panelSearchCriteria.Size = new System.Drawing.Size(926, 100);
             this.panelSearchCriteria.TabIndex = 3;
@@ -134,9 +113,9 @@
             // 
             this.panelSearchResult.Controls.Add(this.pbLoading);
             this.panelSearchResult.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelSearchResult.Location = new System.Drawing.Point(0, 200);
+            this.panelSearchResult.Location = new System.Drawing.Point(0, 150);
             this.panelSearchResult.Name = "panelSearchResult";
-            this.panelSearchResult.Size = new System.Drawing.Size(926, 537);
+            this.panelSearchResult.Size = new System.Drawing.Size(926, 587);
             this.panelSearchResult.TabIndex = 8;
             // 
             // pbLoading
@@ -145,7 +124,7 @@
             this.pbLoading.Image = ((System.Drawing.Image)(resources.GetObject("pbLoading.Image")));
             this.pbLoading.Location = new System.Drawing.Point(0, 0);
             this.pbLoading.Name = "pbLoading";
-            this.pbLoading.Size = new System.Drawing.Size(926, 537);
+            this.pbLoading.Size = new System.Drawing.Size(926, 587);
             this.pbLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pbLoading.TabIndex = 4;
             this.pbLoading.TabStop = false;
@@ -194,6 +173,28 @@
             this.lblExit.Text = "";
             this.lblExit.Click += new System.EventHandler(this.LblExit_Click);
             // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.06936F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbxSearch.Location = new System.Drawing.Point(3, 3);
+            this.tbxSearch.Multiline = true;
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(827, 44);
+            this.tbxSearch.TabIndex = 0;
+            this.tbxSearch.TextChanged += new System.EventHandler(this.TbxSearch_TextChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSearch.Location = new System.Drawing.Point(836, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(87, 44);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Искать";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
             // InfokioskSearchArticleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
@@ -222,7 +223,6 @@
 
         #endregion
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanelSearchCriteria;
-        private System.Windows.Forms.TextBox tbxSearch;
         private System.Windows.Forms.Panel panelSearchCriteria;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rbContent;
@@ -232,6 +232,7 @@
         private System.Windows.Forms.Panel panelToolbar;
         private System.Windows.Forms.Label lblExit;
         private System.Windows.Forms.Label lblBack;
+        private System.Windows.Forms.TextBox tbxSearch;
         private System.Windows.Forms.Button btnSearch;
     }
 }
